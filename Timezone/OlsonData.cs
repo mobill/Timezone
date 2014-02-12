c01259ba52b1be18db8695afa729f16deecfaa48
Bill Langton <blangton@marketvolt.com>
2014-02-12 12:20:34 -0600

    Initial commit to git

---
+++ Timezone/OlsonData.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using TZ4Net;    //  library from http://www.babiej.demon.nl/Tz4Net/main.htm
+                 //  they request a small donation. 
+
+namespace Timezone {
+    class OlsonData {
+
+
+        public void GetData() {
+
+            // Original code from: http://blogs.msdn.com/b/sqlprogrammability/archive/2008/03/18/using-time-zone-data-in-sql-server-2008.aspx
+
+            //  Change the file path to suit your needs
+            StreamWriter sr = File.CreateText(@"c:\Code\Timezone\OlsonData\TZMapping.txt");
+            StreamWriter wsr = File.CreateText(@"c:\Code\Timezone\OlsonData\WindowsTZMapping.txt");
+            StreamWriter tr = File.CreateText(@"c:\Code\Timezone\OlsonData\TZZones.txt");
+
+            //  write the headers
+            sr.WriteLine("ID\tDaylightName\tStandardName\tRawUtcOffset\tOffsetSeconds\tStandardAbbreviation\tWin32Id");
+            wsr.WriteLine("ID\tDaylightName\tStandardName\tRawUtcOffset\tOffsetSeconds\tStandardAbbreviation\tWin32Id");
+            tr.WriteLine("ID\tTransitionStart\tTransitionEnd\tDeltaSeconds\tDST");
+
+            string[] zoneNames = OlsonTimeZone.AllNames;
+
+            for (int i = 0; i < zoneNames.Length; i++) {
+                OlsonTimeZone tz = OlsonTimeZone.GetInstanceFromOlsonName(zoneNames[i].ToString());
+
+                //  Write out the raw Olson data.  There are many...
+                sr.Write(i.ToString() + "\t");
+                sr.Write(tz.DaylightName.Trim() + "\t");
+                sr.Write(tz.StandardName.Trim() + "\t");
+                sr.Write(tz.RawUtcOffset.ToString() + "\t");
+                sr.Write(tz.RawUtcOffset.TotalSeconds.ToString() + "\t");
+                sr.Write(tz.StandardAbbreviation.Trim() + "\t");
+                sr.WriteLine(tz.Win32Id == null ? "" : tz.Win32Id.Trim());
+                
+                //  Dump the data for all records with a Windows Timezone ID
+                if (tz.Win32Id != null) {
+                    wsr.Write(i.ToString() + "\t");
+                    wsr.Write(tz.DaylightName.Trim() + "\t");
+                    wsr.Write(tz.StandardName.Trim() + "\t");
+                    wsr.Write(tz.RawUtcOffset.ToString() + "\t");
+                    wsr.Write(tz.RawUtcOffset.TotalSeconds.ToString() + "\t");
+                    wsr.Write(tz.StandardAbbreviation.Trim() + "\t");
+                    wsr.WriteLine(tz.Win32Id.Trim());
+                }
+
+                DaylightTime[] times = tz.AllTimeChanges;
+                for (int j = 0; j < times.Length; j++) {
+                    //  I don't want all of the historical data, just the current and future rules.
+                    if (times[j].End.Year.CompareTo(2012) >= 0) {
+                        tr.Write(i.ToString() + "\t");
+                        tr.Write(times[j].Start.ToString("yyyy-MM-dd HH:mm:ss") + "\t");
+                        tr.Write(times[j].End.ToString("yyyy-MM-dd HH:mm:ss") + "\t");
+                        tr.Write(times[j] is StandardTime ? "0\t" : times[j].Delta.TotalSeconds.ToString() + "\t");
+                        tr.WriteLine(times[j] is StandardTime ? false.ToString() : true.ToString());
+                    }
+                }
+            }
+
+            // clean up
+            tr.WriteLine();
+            sr.WriteLine();
+            wsr.WriteLine();
+            tr.Close();
+            sr.Close();
+            wsr.Close();
+        }
+    }
+}
---
+++ Timezone/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.IO;
+
+/// <summary>
+/// This program dumps timezone info to text files.
+/// It uses an opensource library to dump Olson Timezone Data to set of tab separated files.
+/// It also dumps the Windows Timezone data to a tab sep file.
+/// Perhaps the code could be prettier but it served my purpose. May it help you as well.
+/// </summary>
+namespace Timezone {
+
+    class Program {
+        static void Main(string[] args) {
+
+            //  Uncomment the next line to look at SystemTimeZones in the debugger.
+            //  IReadOnlyCollection<TimeZoneInfo> timeZones = TimeZoneInfo.GetSystemTimeZones();
+
+            GetTimeZones();
+
+            var olson = new OlsonData();
+            olson.GetData();
+
+            // Give you a chance to see any console output
+            Console.WriteLine("{0}Press Ctrl^C to close the window. {0}", Environment.NewLine);
+
+            Thread.Sleep(10000 * 100000);
+        }
+
+        /// <summary>
+        /// Retrieve and write out Windows Timezone info to a file.
+        /// </summary>
+        static void GetTimeZones() {
+
+            StreamWriter wsr = File.CreateText(@"c:\Code\Timezone\WindowsData\WindowsTZ.txt");
+
+            // Write the header info, tab separated.
+            wsr.WriteLine("ID\tDaylightName\tStandardName\tRawUtcOffset\tDisplayName\tSupportsDST");
+
+            foreach (TimeZoneInfo z in TimeZoneInfo.GetSystemTimeZones()) {
+                Console.WriteLine(z.Id);
+                wsr.Write(z.Id.Trim() + "\t");
+                wsr.Write(z.DaylightName.Trim() + "\t");
+                wsr.Write(z.StandardName.Trim() + "\t");
+                wsr.Write(z.BaseUtcOffset.ToString() + "\t");
+                wsr.Write(z.DisplayName.Trim() + "\t");
+                wsr.WriteLine(z.SupportsDaylightSavingTime.ToString() + "\t");
+                
+                //  I wanted to see the transition data in the debugger to determine usefulness.
+                //  Windows adjustment rules seem only to include
+                //  the time of day that transitions occur and not the date.
+                if (z.SupportsDaylightSavingTime) {
+                    var rules = z.GetAdjustmentRules();
+                }
+            }
+            
+            //  clean up
+            wsr.WriteLine();
+            wsr.Close();
+        }
+    }
+}
